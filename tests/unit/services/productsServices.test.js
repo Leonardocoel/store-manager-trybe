4017dda82788b8059c0f91abfc8214334374620b
Leonardo C <leonardocoel98@gmail.com>
2022-07-03 12:28:13 -0300

    test: products service tests for reading all data and by id

--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -1,10 +1,106 @@
 const sinon = require("sinon");
-const {expect} = require("chai");
+const { expect } = require("chai");
 
-// const productsService = require("../../../services/productsService");
-const connection = require("../../../helpers/connection");
+const productsService = require("../../../services/productsService");
+const productsModel = require("../../../models/productsModel");
 const { allProductsResponse } = require("../../../__tests__/_dataMock");
 
-describe("Products service tests", () => {
- 
+describe("Products services tests", () => {
+  describe("Read tests", () => {
+    describe("Require all data", () => {
+      describe("When the result is undefined", () => {
+        before(async () => {
+          sinon.stub(productsModel, "getAll").resolves();
+        });
+
+        after(async () => {
+          productsModel.getAll.restore();
+        });
+
+        it("Verify if the return is an empty array", async () => {
+          const response = await productsService.getAll();
+
+          expect(response).to.equal([]);
+        });
+      });
+
+      describe("When the result is defined", () => {
+        before(async () => {
+          sinon.stub(productsModel, "getAll").resolves(allProductsResponse);
+        });
+
+        after(async () => {
+          productsModel.getAll.restore();
+        });
+
+        it("Verify if it returns an array of objects", async () => {
+          const response = await productsService.getAll();
+
+          expect(response).to.be.an("array");
+          response.forEach((r) => {
+            expect(r).to.be.an("object");
+            expect(r).to.have.all.keys(["id", "name"]);
+          });
+        });
+
+        it("Verify if array is in ascending order by id", async () => {
+          const response = await productsService.getAll();
+
+          expect(response).to.be.ascendingBy("id");
+        });
+      });
+    });
+
+    describe("Request data by id", () => {
+      describe("When the id is not valid", () => {
+        it("Verify if the id is invalid returns false", async () => {
+          const response = await productsService.getById(2);
+
+          expect(response).to.be.a("boolean").that.equal(false);
+        });
+      });
+
+      describe("When the result is undefined", () => {
+        before(async () => {
+          sinon.stub(productsModel, "getById").resolves();
+        });
+
+        after(async () => {
+          productsModel.getById.restore();
+        });
+
+        it("Verify if the return is an empty array", async () => {
+          const response = await productsService.getById(2);
+
+          expect(response).to.equal([]);
+        });
+      });
+
+      describe("When the result is defined", () => {
+        before(async () => {
+          sinon
+            .stub(productsModel, "getById")
+            .withArgs(2)
+            .resolves(allProductsResponse[1]);
+        });
+
+        after(async () => {
+          productsModel.getById.restore();
+        });
+
+        it("Verify if it returns an object", async () => {
+          const response = await productsService.getById(2);
+
+          expect(response).to.be.an("object");
+        });
+
+        it("Verify if the object has the correct information", async () => {
+          const response = await productsService.getById(2);
+
+          expect(response).to.have.property("id", 2);
+          expect(response).to.be.equal(allProductsResponse[1]);
+        });
+      });
+    });
+  });
 });
