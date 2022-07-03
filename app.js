cb67e65ca5036cbd210449124f1717b6234ff8b0
Leonardo C <leonardocoel98@gmail.com>
2022-07-03 17:01:33 -0300

    fix: products controller tests for reading all data and bd connection

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ const router = require('./routers/index');
 
 const app = express();
 
-app.use(bodyParser);
+app.use(bodyParser.json());
 app.use(helmet());
 app.use(morgan('common'));
+app.use('/products', rescue(router.productsRouter));
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -8,54 +8,56 @@ const { allProductsResponse } = require("../../../__tests__/_dataMock");
 describe("Products controller tests", () => {
   describe("Read request tests", () => {
     describe("Require all data", () => {
-      describe("When the result is undefined", () => {
-        const response = {};
-        const request = {};
+      describe("When the result is undefined", async () => {
+        const res = {};
+        const req = {};
+        const next = sinon.stub().returns();
 
         before(() => {
-          response.status = sinon.stub().returns(response);
-          response.send = sinon.stub().returns;
+          res.status = sinon.stub().returns(res);
+          res.json = sinon.stub().returns();
 
-          sinon.stub(productsService, "getAll").resolves(false);
+          sinon.stub(productsService, "getAll").resolves(undefined);
         });
         after(() => {
           productsService.getAll.restore();
         });
 
-        it("Verifies if the status is called with the code 400", async () => {
-          await productsController.getAll(request, response);
-          expect(response.status.calledWith(400)).to.be.true;
+        it("Verifies if the status is called with the code 404", async () => {
+          await productsController.getAll(req, res, next);
+          expect(res.status.calledWith(404)).to.be.true;
         });
 
-        it('Verifies if the send is called with the message "There are no products"', async () => {
-          await productsController.getAll(request, response);
-          expect(response.send.calledWith("There are no products")).to.be.true;
+        it('Verifies if the json is called with the message "There are no products"', async () => {
+          await productsController.getAll(req, res, next);
+          expect(res.json.calledWith({ message: 'There are no products' })).to.be.true
         });
       });
       describe("When the result is defined", () => {
-        const response = {};
-        const request = {};
+        const req = {};
+        const res = {};
+        const next = sinon.stub().returns();
 
         before(() => {
-          response.status = sinon.stub().returns(response);
-          response.send = sinon.stub().resolves(allProductsResponse);
+          req.status = sinon.stub().returns(res);
+          req.json = sinon.stub();
 
-          sinon.stub(productsService, "getAll").resolves(true);
+          sinon.stub(productsService, "getAll").resolves(allProductsResponse);
         });
         after(() => {
           productsService.getAll.restore();
         });
 
         it("Verifies if the status is called with the code 200", async () => {
-          await productsController.getAll(request, response);
-          expect(response.status.calledWith(200)).to.be.true;
+          await productsController.getAll(res, req, next);
+          expect(req.status.calledWith(200)).to.be.true;
         });
 
         it("Verify if it sends an array of objects", async () => {
-          await productsController.getAll(request, response);
-
-          expect(response.send).to.be.an("array");
-          response.send.forEach((r) => {
+          const result = await productsService.getAll(res, req, next);
+          
+          expect(result).to.be.an("array");
+          result.forEach((r) => {
             expect(r).to.be.an("object");
             expect(r).to.have.all.keys(["id", "name"]);
           });
