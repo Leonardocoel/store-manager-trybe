480384fef3e60c2c1fe26b4975d06316a013a207
Leonardo C <leonardocoel98@gmail.com>
2022-07-03 11:40:50 -0300

    refactor: change string of describe and it in products model test

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -4,57 +4,62 @@ const chai = require("chai");
 const productsModel = require("../../../models/productsModel");
 const connection = require("../../../helpers/connection");
 const { allProductsResponse } = require("../../../__tests__/_dataMock");
-const { beforeEach } = require("mocha");
-const { assert } = require("joi");
 
 const { expect } = chai;
 chai.use(require("chai-sorted"));
 
 describe("Products model tests", () => {
-  describe("Get all data from DB", () => {
-    before(async () => {
-      sinon.stub(connection, "execute").resolves([allProductsResponse]);
-    });
-    after(async () => {
-      connection.execute.restore();
-    });
-    it("the returned data is an array of objects", async () => {
-      const response = await productsModel.getAll();
+  describe("Database read tests", () => {
+    describe("Require all table data", () => {
+      before(async () => {
+        sinon.stub(connection, "execute").resolves([allProductsResponse]);
+      });
 
-      expect(response).to.be.an("array");
-      response.forEach((r) => {
-        expect(r).to.be.an("object");
-        expect(r).to.have.all.keys(["id", "name"]);
+      after(async () => {
+        connection.execute.restore();
+      });
+
+      it("Verify if it returns an array of objects", async () => {
+        const response = await productsModel.getAll();
+
+        expect(response).to.be.an("array");
+        response.forEach((r) => {
+          expect(r).to.be.an("object");
+          expect(r).to.have.all.keys(["id", "name"]);
+        });
+      });
+
+      it("Verify if array is in ascending order by id", async () => {
+        const response = await productsModel.getAll();
+        expect(response).to.be.ascendingBy("id");
       });
     });
-    it("the returned array is in ascending order by id", async () => {
-      const response = await productsModel.getAll();
-      expect(response).to.be.ascendingBy("id");
-    });
-  });
+    describe("Request table data by id", () => {
+      before(async () => {
+        const select = `SELECT * FROM StoreManager.products\n    WHERE id = ?`;
 
-  describe("Get data by id from DB", () => {
-    before(async () => {
-      const select = `SELECT * FROM StoreManager.products\n    WHERE id = ?`;
-      sinon
-        .stub(connection, "execute")
-        .withArgs(select, [2])
-        .resolves([allProductsResponse[1]]);
-    });
-    after(async () => {
-      connection.execute.restore();
-    });
-    it("return an object", async () => {
-      const response = await productsModel.getById(2);
+        sinon
+          .stub(connection, "execute")
+          .withArgs(select, [2])
+          .resolves([allProductsResponse[1]]);
+      });
 
-      expect(response).to.be.an("object");
-    });
+      after(async () => {
+        connection.execute.restore();
+      });
 
-    it("verify if object id is correct", async () => {
-      const response = await productsModel.getById(2);
+      it("Verify if it returns an object", async () => {
+        const response = await productsModel.getById(2);
 
-      expect(response).to.have.property("id", 2);
-      expect(response).to.be.equal(allProductsResponse[1]);
+        expect(response).to.be.an("object");
+      });
+
+      it("Verify if the object has the correct information", async () => {
+        const response = await productsModel.getById(2);
+
+        expect(response).to.have.property("id", 2);
+        expect(response).to.be.equal(allProductsResponse[1]);
+      });
     });
   });
 });
