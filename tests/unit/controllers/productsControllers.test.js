e62c72d0c12e3a9f768d4a40696079c9abbac37f
Leonardo C <leonardocoel98@gmail.com>
2022-07-03 17:25:56 -0300

    test: test controller requesting data by id

--- tests/unit/controllers/productsControllers.test.js
+++ tests/unit/controllers/productsControllers.test.js
@@ -54,7 +54,7 @@ describe("Products controller tests", () => {
         });
 
         it("Verify if it sends an array of objects", async () => {
-          const result = await productsService.getAll(res, req, next);
+          const result = await productsService.getAll();
           
           expect(result).to.be.an("array");
           result.forEach((r) => {
@@ -64,5 +64,69 @@ describe("Products controller tests", () => {
         });
       });
     });
+    describe("Require data by id", () => {
+      describe("When the result is undefined", async () => {
+        const res = {};
+        const req = {};
+        const next = sinon.stub().returns();
+
+        before(() => {
+          res.status = sinon.stub().returns(res);
+          res.json = sinon.stub().returns();
+
+          sinon.stub(productsService, "getById").resolves(undefined);
+        });
+        after(() => {
+          productsService.getAll.restore();
+        });
+
+        it("Verifies if the status is called with the code 404", async () => {
+          await productsController.getById(req, res, next);
+          expect(res.status.calledWith(404)).to.be.true;
+        });
+
+        it('Verifies if the json is called with the message "Product not found"', async () => {
+          await productsController.getById(req, res, next);
+          expect(res.json.calledWith({ message: "Product not found"  })).to.be.true
+        });
+      });
+      describe("When the result is defined", () => {
+        const req = {};
+        const res = {};
+        const next = sinon.stub().returns();
+
+        before(() => {
+          req.status = sinon.stub().returns(res);
+          req.json = sinon.stub();
+          req.params = { id: 2 };
+
+          sinon.stub(productsService, "getById")
+            .withArgs(2)
+            .resolves(allProductsResponse[1]);
+        });
+        after(() => {
+          productsService.getAll.restore();
+        });
+
+        it("Verifies if the status is called with the code 200", async () => {
+          await productsController.getById(res, req, next);
+          expect(req.status.calledWith(200)).to.be.true;
+        });
+
+        it("Verify if it returns an object", async () => {
+          const response = await productsService.getById();
+
+          expect(response).to.be.an("object");
+        });
+
+        it("Verify if the object has the correct information", async () => {
+          const response = await productsService.getById();
+
+          expect(response).to.have.property("id", 2);
+          expect(response).to.be.equal(allProductsResponse[1]);
+        });
+        });
+      });
+    })
   });
-});
+
