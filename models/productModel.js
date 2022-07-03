51c5860c323036f3e447a880f6efa7e8a11474af
Leonardo C <leonardocoel98@gmail.com>
2022-07-02 22:01:41 -0300

    test: implement get by id test

---
+++ models/productModel.js
@@ -0,0 +1,6 @@
+const connection = require('../helpers/connection');
+
+  module.exports = {
+    getAll,
+    getById,
+};
--- tests/unit/models/productModels.test.js
+++ tests/unit/models/productModels.test.js
@@ -8,17 +8,15 @@ const { allProductsResponse } = require("../../../__tests__/_dataMock");
 const { expect } = chai;
 chai.use(require("chai-sorted"));
 
-describe("Get all data from DB", () => {
-  before(async () => {
-    sinon.stub(connection, "execute").resolves(allProductsResponse);
-  });
-
-  after(async () => {
-    connection.execute.restore();
-  });
-
-  describe("When it is read successfully", () => {
-    it("return an array of objects in ascending order", async () => {
+describe("Product model tests", () => {
+  describe("Get all data from DB", () => {
+    before(async () => {
+      sinon.stub(connection, "execute").resolves([allProductsResponse]);
+    });
+    after(async () => {
+      connection.execute.restore();
+    });
+    it("the returned data is an array of objects", async () => {
       const response = await productModel.getAll();
 
       expect(response).to.be.an("array");
@@ -26,7 +24,31 @@ describe("Get all data from DB", () => {
         expect(r).to.be.an("object");
         expect(r).to.have.all.keys(["id", "name"]);
       });
+    });
+    it("the returned array is in ascending order by id", async () => {
+      const response = await productModel.getAll();
       expect(response).to.be.ascendingBy("id");
+    });
+  });
+
+  describe("Get data by id from DB", () => {
+    before(async () => {
+      sinon.stub(connection, "execute").resolves([allProductsResponse[1]]);
+    });
+    afterEach(async () => {
+      connection.execute.restore();
+    });
+    it("return an object", async () => {
+      const response = await productModel.getById(2);
+
+      expect(response).to.be.an("object");
+    });
+
+    it("verify if object id is correct", async () => {
+      const response = await productModel.getById(2);
+
+      expect(response).to.have.property("id", 2);
+      expect(response).to.be.equal(allProductsResponse[1]);
     });
   });
 });
