570fe4f95cfbc9a4fef8e359599efdae157e11ac
Leonardo C <leonardocoel98@gmail.com>
2022-07-02 22:02:58 -0300

    feat: create getAll and getById functions

--- models/productModel.js
+++ models/productModel.js
@@ -1,5 +1,21 @@
 const connection = require('../helpers/connection');
 
+const getAll = async () => {
+  const [rows] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+  return rows;
+};
+
+const getById = async (id) => {
+  const [rows] = await connection.execute(
+    `SELECT * FROM StoreManager.products
+    WHERE id = ?`,
+    [id],
+  );
+  return rows;
+};
+
   module.exports = {
     getAll,
     getById,
